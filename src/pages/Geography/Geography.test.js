9da18acdac214b272c15b7535dcae87433dcc961
Stefan Hochwarter <stefan.hochwarter@gmail.com>
2018-02-09 14:17:29 -0500

    added Geography page tests

---
+++ src/pages/Geography/Geography.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Geography from './';
+
+describe('Page Geography', () => {
+  it('renders correctly', () => {
+    const wrapper = shallow(<Geography />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('sets meausreId correctly', () => {
+    const wrapper = shallow(<Geography />);
+    wrapper.instance().setMeasureId('333');
+    expect(wrapper.state()).toMatchObject({ measureId: '333' });
+  });
+
+  it('set geographicTypeId correctly', () => {
+    const wrapper = shallow(<Geography />);
+    wrapper.instance().setGeographicTypeId('1');
+    expect(wrapper.state()).toMatchObject({ geographicTypeId: '1' });
+  });
+
+  it('handles input geographicRollup check correctly', () => {
+    const wrapper = shallow(<Geography />);
+    const event = {
+      target: {
+        type: 'checkbox',
+        checked: true,
+        name: 'geographicRollup'
+      }
+    };
+    wrapper.instance().handleInputChange(event);
+    expect(wrapper.state()).toMatchObject({ geographicRollup: true });
+  });
+
+  it('handle input change correctly (ids)', () => {
+    const wrapper = shallow(<Geography />);
+    const event = {
+      target: {
+        value: '21',
+        name: 'measureId'
+      }
+    };
+    wrapper.instance().handleInputChange(event);
+    expect(wrapper.state()).toMatchObject({ measureId: '21' });
+  });
+});
--- src/pages/Geography/index.js
+++ src/pages/Geography/index.js
@@ -17,7 +17,7 @@ class Geography extends Component {
     this.setGeographicTypeId = this.setGeographicTypeId.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
   }
-  
+
   setMeasureId(measureId) {
     this.setState({ measureId });
   }
@@ -38,21 +38,37 @@ class Geography extends Component {
   render() {
     const { measureId, geographicTypeId, geographicRollup } = this.state;
     const validUrl = measureId !== null && geographicTypeId !== null;
-    const url = `https://ephtracking.cdc.gov/apigateway/api/v1/geography/${measureId}/${geographicTypeId}/${geographicRollup ? 1 : 0}`
+    const url = `https://ephtracking.cdc.gov/apigateway/api/v1/geography/${measureId}/${geographicTypeId}/${
+      geographicRollup ? 1 : 0
+    }`;
     return (
       <Fragment>
-        <h1 className="title">Retrieving all Geographies for a MeasureId, GeographicType and Geographic Rollup</h1>
+        <h1 className="title">
+          Retrieving all Geographies for a MeasureId, GeographicType and
+          Geographic Rollup
+        </h1>
         <h5 className="title is-5">Usage</h5>
         <CodeBlock>
-          https://ephtracking.cdc.gov/apigateway/api/{'{'}version{'}'}/geography/{'{'}measureId{'}'}/{'{'}geographicTypeId{'}'}/{'{'}geographicRollup{'}'}[?apiToken]
+          https://ephtracking.cdc.gov/apigateway/api/{'{'}version{'}'}/geography/{
+            '{'
+          }measureId{'}'}/{'{'}geographicTypeId{'}'}/{'{'}geographicRollup{'}'}[?apiToken]
         </CodeBlock>
         <hr />
         <h5 className="title is-5">Set parameters</h5>
         <CIM handleSelect={this.setMeasureId} />
-        <SelectGeographicType handleSelect={this.setGeographicTypeId} measureId={measureId} />
+        <SelectGeographicType
+          handleSelect={this.setGeographicTypeId}
+          measureId={measureId}
+        />
         <div className="field">
           <label className="checkbox">
-            <input name="geographicRollup" type="checkbox" checked={this.state.geographicRollup} onChange={this.handleInputChange} className="input-checkbox" />
+            <input
+              name="geographicRollup"
+              type="checkbox"
+              checked={this.state.geographicRollup}
+              onChange={this.handleInputChange}
+              className="input-checkbox"
+            />
             geographicRollup
           </label>
         </div>
