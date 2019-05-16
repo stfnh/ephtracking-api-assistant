d75ac08ece527cd028076f7d58554aca58675ff7
Stefan Hochwarter <stefan.hochwarter@gmail.com>
2019-05-16 18:07:59 +0200

    updated tests

--- src/pages/Contentareas/Contentareas.test.js
+++ src/pages/Contentareas/Contentareas.test.js
@@ -8,22 +8,4 @@ describe('Page Contentareas', () => {
     const wrapper = shallow(<Contentareas />);
     expect(wrapper).toMatchSnapshot();
   });
-
-  it('handle getChildMeasure check', () => {
-    const wrapper = shallow(<Contentareas />);
-    const event = {
-      target: { type: 'checkbox', checked: true, name: 'getChildMeasure' }
-    };
-    wrapper.instance().handleInputChange(event);
-    expect(wrapper.state()).toMatchObject({ getChildMeasure: true });
-  });
-
-  it('handle getMultiMeasure check', () => {
-    const wrapper = shallow(<Contentareas />);
-    const event = {
-      target: { type: 'checkbox', checked: true, name: 'getMultiMeasure' }
-    };
-    wrapper.instance().handleInputChange(event);
-    expect(wrapper.state()).toMatchObject({ getMultiMeasure: true });
-  });
 });
